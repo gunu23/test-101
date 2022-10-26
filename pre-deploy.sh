d6a77dfa659d6ffed173ce7d55cac4efd6e271a4
Pranjal Patil <pranjalpatil@ibm.com>
2022-10-26 14:03:22 -0400

    upd jenkinsfile2

--- pre-deploy.sh
+++ pre-deploy.sh
@@ -1,21 +1,21 @@
   #!/bin/bash
     
-    //Install operator
-    oc apply -f ibm-catalog-source.yaml
+#Install operator
+  oc apply -f ibm-catalog-source.yaml
 
-    //Install DataPower Operator - TBD
+#Install DataPower Operator - TBD
 
-    //create new project
-    oc new-project sce-test
+#create new project
+  oc project sce-test
 
-    //create an IBM Entitlement Key - TBD
-    oc create secret docker-registry ibm-entitlement-key -n tools \
-    --docker-username=cp \
-    --docker-password="$TOKEN" \
-    --docker-server=cp.icr.io
+#create an IBM Entitlement Key - TBD
+  oc create secret docker-registry ibm-entitlement-key -n tools \
+  --docker-username=cp \
+  --docker-password="$TOKEN" \
+  --docker-server=cp.icr.io
 
-    //Create Admin User credential secret - TBD
+#Create Admin User credential secret - TBD
 
-    //create secrets for keys and certs - TBD
+#create secrets for keys and certs - TBD
 
-    //Passing the namespace into migrate-backup.sh - TBD
+#Passing the namespace into migrate-backup.sh - TBD
